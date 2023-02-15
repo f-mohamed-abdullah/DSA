960cbe1e858c03e5deb6e4bb8b589f9f4c3e0ea9
F.Mohamed Abdullah <abdullahfakrudeen2020@gmail.com>
2023-02-15 22:41:10 +0530

    New Repo

---
+++ src/com/Abu/Arrays/nass.java
@@ -0,0 +1,11 @@
+package com.Abu.Arrays;
+
+import java.util.Arrays;
+
+public class nass {
+    public static void main(String[] args) {
+        int n=123;
+        String[] i= new String[]{Integer.toString(n)};
+        System.out.println(Arrays.toString(i));
+    }
+}
---
+++ src/com/Abu/Searching/Linear_Search.java
@@ -0,0 +1,39 @@
+package com.Abu.Searching;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Linear_Search {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+
+        System.out.print("Enter how many elements in array:");
+        int ele=in.nextInt();
+        int[] arr=new int[ele];
+
+        //Getting values from the user for Array
+        for (int i = 0; i < ele; i++) {
+            System.out.print("Enter "+(i+1)+" value:");
+            arr[i]= in.nextInt();
+        }
+        //The Array
+        System.out.println(Arrays.toString(arr));
+
+        System.out.print("Enter the value to search in Array:");
+        int key=in.nextInt();
+
+        int pos=linear(arr,key);
+        System.out.println("Value found in "+pos+" position");
+
+
+    }
+
+    private static int linear(int[] arr,int key){
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==key){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+}
